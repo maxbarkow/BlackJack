2a10ddb85de208e36edfefc821bf367cbf8f85d5
Max Barkow <maxbarkow@lornes-macbook-air.local>
2017-12-08 14:14:06 -0500

    Initial Commit

---
+++ BlackJack Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BlackJack Game
+//
+//  Created by Lorne Barkow on 12/8/17.
+//  Copyright © 2017 Max Barkow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
